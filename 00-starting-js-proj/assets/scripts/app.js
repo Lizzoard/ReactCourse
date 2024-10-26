6ed1b27a4808719e275d0182e98206f16507deda
Lizzoard <lizzoard82@gmail.com>
2024-10-26 08:27:27 +0200

    Added example functions

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -2,4 +2,25 @@
 
 import apiKey from "./utils.js";
 
-console.log(apiKey);
+// console.log(apiKey);
+
+// const userMessage = "Hello User!!!";
+
+// console.log(userMessage);
+
+// function greet() {   
+//   console.log("Hello");
+// }
+
+// function greetUser(userName, message="Hello!") {
+//   console.log(userName);
+//   console.log(message);
+// }
+
+// function greetUser(userName, message = "Bye", status) {
+//   return "Hi, welcome" + userName + ". " + message + " your status is " + status;
+// }
+
+// greetUser("max", "hello");
+// greetUser("sam", "good to see you", true);
+// greet();
