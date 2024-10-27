c9d9c8131f8e9e9f701eceb0a61f48ebd0f8507f
Lizzoard <lizzoard82@gmail.com>
2024-10-27 07:36:14 +0100

    Added arrays and some possibilities to modify them

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -24,3 +24,27 @@ import apiKey from "./utils.js";
 // greetUser("max", "hello");
 // greetUser("sam", "good to see you", true);
 // greet();
+
+// const hobbies = ["Sports", "Cooking", "Movies"];
+// console.log(hobbies[0]);
+
+// hobbies.push("Working");
+// console.log(hobbies);
+
+// const index = hobbies.findIndex((item) => item === "Cooking");
+
+// console.log(index);
+
+// const editedHobbies = hobbies.map((item) => ({ name : item }));
+// console.log(editedHobbies);
+
+// const userNameData = ["Björn", "Bauer"];
+
+// const firstName = userNameData[0];
+// const lastName = userNameData[1];
+
+// const [firstName, lastName] = ["Lillith", "Bauer"];
+
+// console.log(firstName);
+// console.log(lastName);
+ 
