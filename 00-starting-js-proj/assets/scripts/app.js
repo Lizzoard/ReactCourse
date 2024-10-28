c3faff103653ae6305605b78ff7a024d191a60d6
Lizzoard <lizzoard82@gmail.com>
2024-10-28 06:38:45 +0100

    short correction done

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -115,23 +115,31 @@ import apiKey from "./utils.js";
 
 // Timeout function and usage
 
-function handleTimeout() {
-  console.log("Timed out!");
-}
+// function handleTimeout() {
+//   console.log("Timed out!");
+// }
 
-const handleTimeout2 = () => {
-  console.log("Timed out ... again!");
-};
+// const handleTimeout2 = () => {
+//   console.log("Timed out ... again!");
+// };
 
-setTimeout(handleTimeout, 2000);
-setTimeout(handleTimeout2, 3000);
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
 
-setTimeout(() => {
-  console.log("More timing out...");
-}, 4000);
+// setTimeout(() => {
+//   console.log("More timing out...");
+// }, 4000);
 
-function greeter(greetFn) {
-  greetFn();
-}
+// function greeter(greetFn) {
+//   greetFn();
+// }
 
-greeter(() => console.log("Hi"));
+// greeter(() => console.log("Hi"));
+
+// function init() {
+//   function greet() {
+//     console.log("hi");
+//   }
+//   greet();
+// }
+
