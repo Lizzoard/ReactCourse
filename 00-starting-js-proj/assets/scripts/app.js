4f9104e511a40416b50c95d5da90141a125de810
Lizzoard <lizzoard82@gmail.com>
2024-10-28 06:38:01 +0100

    example for timeout functions added

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -113,3 +113,25 @@ import apiKey from "./utils.js";
 //   console.log(hobby);
 // }
 
+// Timeout function and usage
+
+function handleTimeout() {
+  console.log("Timed out!");
+}
+
+const handleTimeout2 = () => {
+  console.log("Timed out ... again!");
+};
+
+setTimeout(handleTimeout, 2000);
+setTimeout(handleTimeout2, 3000);
+
+setTimeout(() => {
+  console.log("More timing out...");
+}, 4000);
+
+function greeter(greetFn) {
+  greetFn();
+}
+
+greeter(() => console.log("Hi"));
