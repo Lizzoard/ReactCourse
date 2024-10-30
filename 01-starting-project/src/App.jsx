fac133229153b6675f9065bda80f0a9a85104c73
Lizzoard <lizzoard82@gmail.com>
2024-10-30 08:12:30 +0100

    next component with the use of props
    A second component was added and the different possibilities to use props where discussed and shown,
    I added all possible ways to use props as commented code in App.jsx

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,7 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from "./data";
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -8,7 +12,7 @@ function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         { description } React concepts you will need for almost any app you are
@@ -18,12 +22,50 @@ function Header() {
   );
 }
 
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description }</p>
+//     </li>
+//   );
+// }
+
+function CoreConcept({ image, title, description }) { // using destructuring
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            {/* <CoreConcept
+              title="Components"
+              description="The core ui building block"
+              image={componentsImg}
+            /> */}
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title} 
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+              /> */}
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
