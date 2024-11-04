6d5800bf28e94c5874df88c1266fe3e0178360c0
Lizzoard <lizzoard82@gmail.com>
2024-11-04 16:07:02 +0100

    Created player component

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  
+  function toggleEdit() {
+    setIsEditing(editing => !isEditing); 
+  }
+
+  function handleChange(event) {
+        setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && <input type="text" required value={playerName} onChange={handleChange}/>}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={toggleEdit}>{!isEditing ? "Edit" : "Save" }</button>
+    </li>
+  );
+}
