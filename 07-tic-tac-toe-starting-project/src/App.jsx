219e4467342abcb5cf72d0353f291b3bcc321f0b
Lizzoard <lizzoard82@gmail.com>
2024-11-06 20:35:43 +0100

    Changed Player tracking logic and added Log component

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,12 +2,27 @@ import { useState } from "react";
 
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
- function handleSelectSquare() {
+ function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
+   setGameTurns(prevTurns => {
+     let currentPlayer = 'X';
+
+     if (prevTurns.length >0 && prevTurns[0].player === 'X') {
+       currentPlayer = 'O';
+     }
+
+     const updatedTurns =
+       [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }
+         , ...prevTurns];
+     
+     return updatedTurns;
+   }); 
   }
 
   return (
@@ -20,14 +35,14 @@ function App() {
             isActive={activePlayer === "X"}
           />
           <Player
-            initialName="Player2"
+            initialName="Player2" 
             symbol="O"
             isActive={activePlayer === "O"}
           />
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
-      log
+      <Log />
     </main>
   );
 }
